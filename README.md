15aae67c01d9fb0d26fe33305c58d574eb7b7293
Joyce Babu <joyce@ennexa.com>
2020-02-28 16:37:50 +0530

    fix formatting

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # uiohook-sys
+
 System crate for building and linking `libuiohook`
+
+## Requirements
+
+On ubuntu required packages can be installed with
+
+    apt install -y libxinerama-dev libxtst-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libxkbfile-dev clang libdbus-1-dev gcc pkg-config
--- build.rs
+++ build.rs
@@ -7,12 +7,12 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
-	let target = env::var("TARGET").expect("TARGET was not set");
+    let target = env::var("TARGET").expect("TARGET was not set");
 
     println!("cargo:rerun-if-changed=build.rs");
 
     if cfg!(feature = "with-bindgen") {
-    	let bindings = bindgen::Builder::default()
+        let bindings = bindgen::Builder::default()
             .header("vendor/include/uiohook.h")
             .whitelist_var("_event_type_EVENT.*")
             .whitelist_var("_log_level_LOG_LEVEL.*")
@@ -30,21 +30,22 @@ fn main() {
             .whitelist_function("hook_get_pointer_sensitivity")
             .whitelist_function("hook_get_multi_click_time")
             .trust_clang_mangling(false)
+            .rustfmt_bindings(true)
+            .derive_debug(false)
             .generate()
-			.expect("Unable to generate bindings");
+            .expect("Unable to generate bindings");
 
-		let out_path = PathBuf::from("src").join("bindings.rs");
+        let out_path = PathBuf::from("src").join("bindings.rs");
 
-		let data = bindings
-			.to_string()
-			.replace("_event_type_EVENT", "EVENT")
-			.replace("_log_level_LOG_LEVEL", "LOG_LEVEL")
-		;
+        let data = bindings
+            .to_string()
+            .replace("_event_type_EVENT", "EVENT")
+            .replace("_log_level_LOG_LEVEL", "LOG_LEVEL");
 
         let mut file = File::create(out_path).expect("couldn't open file!");
         file.write_all(data.as_bytes())
             .expect("couldn't write bindings.rs!");
-	}
+    }
 
     if cfg!(feature = "static") {
         if !Path::new("vendor/.git").exists() {
@@ -53,15 +54,16 @@ fn main() {
                 .status();
         }
 
-	    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-	    let include = dst.join("include");
-	    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        let include = dst.join("include");
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
         fs::create_dir_all(&include).unwrap();
         fs::copy("vendor/include/uiohook.h", include.join("uiohook.h")).unwrap();
 
-        if !Path::new("vendor/.libs").exists() {
-            let mut args = vec!["--enable-demo=no"];
+        let libdir = dst.join(".libs");
+        if !libdir.exists() {
+            let mut args = vec!["--enable-demo=no", "--with-carbon-legacy=no"];
 
             if target.contains("musl") {
                 env::set_var("CC", "musl-gcc");
@@ -76,12 +78,7 @@ fn main() {
             }
 
             Command::new("autoreconf")
-                .args(&mut vec![
-                	"--install",
-                	"--verbose",
-                	"--force",
-                	"./vendor"
-                ])
+                .args(&mut vec!["--install", "--verbose", "--force", "./vendor"])
                 .output()
                 .unwrap();
 
@@ -94,18 +91,16 @@ fn main() {
             Command::new("make").current_dir(&dst).output().unwrap();
         }
 
-        let libdir = dst.join(".libs");
 
-	    println!("cargo:rustc-link-search={}", libdir.display());
-	    println!("cargo:rustc-link-lib=static=uiohook");
+        println!("cargo:rustc-link-search={}", libdir.display());
+        println!("cargo:rustc-link-lib=static=uiohook");
 
-	    if target.contains("darwin") {
-		    println!("cargo:rustc-link-lib=framework=IOKit");
-		    println!("cargo:rustc-link-lib=framework=Carbon");
-	    }
-	    // println!("cargo:root={}", env::var("OUT_DIR").unwrap());
+        if target.contains("darwin") {
+            println!("cargo:rustc-link-lib=framework=IOKit");
+            println!("cargo:rustc-link-lib=framework=Carbon");
+        }
+    // println!("cargo:root={}", env::var("OUT_DIR").unwrap());
     } else {
         println!("cargo:rustc-link-lib=uiohook");
     }
-
 }
